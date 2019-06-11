b981657f293b67a2ade628f5ec6085091b30d96f
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-11 21:10:05 +0530

    Update cc.py

--- cc.py
+++ cc.py
@@ -54,56 +54,18 @@ def forawrd(client, message):
     client.send_sticker(int(u),'CAADBQADHwAD271NHQtXw-moeKYWAg')
     client.send_message(int(u),'🍾 **INNINIGS BREAK** 🍾' )
     
-@app.on_message(Filters.chat(int(s)) & Filters.edited & Filters.text)
-def edit(client, message):
-    mess = client.iter_history(int(u), limit=15)
-    for i in mess:
-        x = ' '.join(i.text.split(' ')[0:2]) 
-        y = ' '.join(message.text.split(' ')[0:2])
-
-        if x.casefold() in y.casefold():
-          text = message.text
-          f = False
-          words = ['lund','dekho','fix','fixer','👆','👇','sab','karvana','link','loss','audio','varna','pura','puri','open','paid','contact','baazigar','whatsapp','timepass','kamma','book','teenpatti','diya','bhai','🥺','members','🖕','member','only','chut','lund','gand','ma','maa','bhosdi','bahan','loude','lode','lavde','chutiya','🤞','🤟','☝️','mkc','bkc','mc','bc','madarchod','bahanchod','bahnchod','gandu','❓','kya','wbt','line','who',"https://",'joinchat','bullet','fuck','🤔']
-          for word in words:
-            if word.casefold() in text.casefold():
-              f = True
-          if not f:       
-           if '🖲' in text:
-            i.edit(message.text.replace('🖲' , '**💘'))
-           elif '📟' in text:
-            i.edit(message.text.replace('📟' , '🏝'))
-           elif 'Runs Scored:' in text:
-            i.edit(message.text.replace('Runs Scored:' , '**Runs Scored:**'))
-           elif 'RUNS SCORED:' in text:
-            i.edit(message.text.replace('RUNS SCORED:' , '**Runs Scored:**'))
-           elif '🎾' in text:
-            i.edit(message.text.replace('🎾' , '🥎'))
-           else:
-            i.edit(message.text)
-
-
-
-        elif y.casefold() in x.casefold():
-          text = message.text
-          f = False
-          words = ['lund','dekho','fix','fixer','👆','👇','sab','karvana','link','loss','audio','varna','pura','puri','open','paid','contact','baazigar','whatsapp','timepass','kamma','book','teenpatti','diya','bhai','🥺','members','🖕','member','only','chut','lund','gand','ma','maa','bhosdi','bahan','loude','lode','lavde','chutiya','🤞','🤟','☝️','mkc','bkc','mc','bc','madarchod','bahanchod','bahnchod','gandu','❓','kya','wbt','line','who',"https://",'joinchat','bullet','fuck','🤔']
-          for word in words:
-            if word.casefold() in text.casefold():
-              f = True
-          if not f:       
-           if '🖲' in text:
-            i.edit(message.text.replace('🖲' , '**💘'))
-           elif '📟' in text:
-            i.edit(message.text.replace('📟' , '🏝'))
-           elif 'Runs Scored:' in text:
-            i.edit(message.text.replace('Runs Scored:' , '**Runs Scored:**'))
-           elif 'RUNS SCORED:' in text:
-            i.edit(message.text.replace('RUNS SCORED:' , '**Runs Scored:**'))
-           elif '🎾' in text:
-            i.edit(message.text.replace('🎾' , '🥎'))
-           else:
-            i.edit(message.text)
+@app.on_message(Filters.chat(int(s))& Filters.text & Filters.edited)
+def forward(client, message):
+  print(message.text)
+  file = open("sure.txt" , "r")
+  lines = file.readlines()
+  file.close()
+  for line in lines:
+   x = line.split()
+   id = str(message.message_id)
+   if id in x:
+     client.edit_message_text(int(u),int(x[x.index(id)+1]), message.text)
+  
         
 app.run()
 
